92353bc99ae738f5611fec8c7567fc5a9cf5e636
Ronnie Skansing <rskansing@gmail.com>
2015-08-28 18:32:59 +0000

    more events

--- Test.php
+++ Test.php
@@ -9,6 +9,7 @@ use Composer\Plugin\PluginInterface;
 use Composer\Plugin\PluginEvents;
 use Composer\Plugin\PreFileDownloadEvent;
 
+echo 'Plugin Loaded ' . \PHP_EOL;
 
 class Test implements PluginInterface, EventSubscriberInterface {
   protected $composer;
@@ -16,22 +17,32 @@ class Test implements PluginInterface, EventSubscriberInterface {
 
   public function activate(Composer $composer, IOInterface $io)
   {
+    echo 'Activate event ' . \PHP_EOL;
     $this->composer = $composer;
     $this->io = $io;
   }
 
   public static function getSubscribedEvents()
   {
+    echo 'getSubscribedEvents event ' . \PHP_EOL;
     return [
       PluginEvents::COMMAND => [
         ['onCommand', 0]
+      ],
+      PluginEvents::PRE_FILE_DOWNLOAD => [
+        ['onPreFileDownload', 0]
       ]
     ];
   }
 
   public function onCommand(CommandEvent $event)
   {
-    var_dump(`whoami`, $event, $event->commandName); die;
+    echo 'onCommandevent ' . \PHP_EOL;
+    var_dump(`whoami`, $event, $event->getCommandName());
+  }
+
+  public function onPreFileDownload(PreFileDownloadEvent $event) {
+    echo 'onPreFileDownload event ' . \PHP_EOL;
   }
 
 }
