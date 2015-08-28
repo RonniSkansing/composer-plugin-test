fe36e498d0505b443213caea793f95e4a55f6abf
Ronnie Skansing <rskansing@gmail.com>
2015-08-28 18:22:11 +0000

    Inspecting command interface

--- Test.php
+++ Test.php
@@ -1,14 +1,36 @@
 <?php
 namespace Skansing;
 
-
-exec('whoami') === 'root' && die('Aborting Sudo not allowd');
-
-
 use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
 use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
+use Composer\Plugin\PluginEvents;
+use Composer\Plugin\PreFileDownloadEvent;
 
-class Test implements PluginInterface {
-  public function activate(Composer $composer, IOInterface $io){}
+
+class Test implements PluginInterface, EventSubscriberInterface {
+  protected $composer;
+  protected $io;
+
+  public function activate(Composer $composer, IOInterface $io)
+  {
+    $this->composer = $composer;
+    $this->io = $io;
+  }
+
+  public static function getSubscribedEvents()
+  {
+    return [
+      PluginEvents::COMMAND => [
+        ['onCommand', 0]
+      ]
+    ];
+  }
+
+  public function onCommand(CommandEvent $event)
+  {
+    var_dump(`whoami`, $event, $event->commandName); die;
+  }
+
 }
