339e9f1f3e5f2754926cbd60a94b204298ba1d9b
Ronnie Skansing <rskansing@gmail.com>
2015-08-28 16:49:22 +0000

    more boilerplate needed

--- Test.php
+++ Test.php
@@ -1,8 +1,14 @@
 <?php
 namespace Skansing;
 
+
 exec('whoami') === 'root' && die('Aborting Sudo not allowd');
 
 
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
 
-class Test {} 
+class Test implements PluginInterface {
+  public function activate(Composer $composer, IOInterface $io){}
+}
